def26ed7f1b13075fcb8c8ee0ec964fab4d1a6d9
Oleksii <oleksii.matiunin@ukad-group.com>
2020-06-15 17:53:46 +0300

    first commit

--- src/enums/ChargeStatuses.ts
+++
@@ -1,6 +0,0 @@
-export const enum ChargeStatuses {
-    Pending = 0,
-    Succeeded = 1,
-    Failed = 2,
-    Canceled = 3
-}
---
+++ src/enums/ContractStatuses.ts
@@ -0,0 +1,6 @@
+export const enum ChargeStatuses {
+    Pending = 0,
+    Succeeded = 1,
+    Failed = 2,
+    Canceled = 3
+}
